26e5aa7565eb1ab28ab65a22b186c9a2ea721c50
小沙盒工作室-室长 <29478722+2439905184@users.noreply.github.com>
2022-07-05 14:10:57 +0800

    这个是给linux用户的，创建新工程的脚本

---
+++ suika2_dashboard.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo please input your project name
+echo 请输入你的工程名字
+
+read project_name
+mkdir $project_name
+
+cd $project_name
+mkdir bg
+mkdir bgm
+mkdir cg
+mkdir ch
+mkdir conf
+mkdir cv
+mkdir font
+mkdir sav
+mkdir se 
+mkdir txt
+cd conf
+touch config.txt
+cd ..
+cd txt
+touch init.txt
+cd ..
+echo empty project has beeen created!
+echo please copy suika2 binary to project floder!
+echo suika2 src is at here:https://github.com/ktabata/suika2
+echo for linux user,we must compile suika2 by ourselfs
+echo but we also can use wine to run suika2!
